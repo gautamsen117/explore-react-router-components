7940e112ad677844717aa63b4d484d99f690a60b
gautamsen117 <gautamsen117@gmail.com>
2022-11-16 18:26:32 +0600

    explore react router components

---
+++ src/components/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <h2>Lot's of Product here</h2>
+        </div>
+    );
+};
+
+export default Products;
